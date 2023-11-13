118550279077f83b30a7f4aa326239d522ee877c
AshwinNair <ashbe404@gmail.com>
2023-11-14 01:13:04 +0530

    Minor fix

--- src/contexts/types.ts
+++ src/contexts/types.ts
@@ -1,7 +1,7 @@
 export interface IEmployeeContextProps {
   sortConfig: { sortColumn: string; sortOrder: string };
   updateSortConfig: (sortColumn: string) => void;
-  filters: { skills: string[]; search: string[] };
-  updateFilters: ({ skills, search }: { skills?: string[]; search?: string[] }) => void;
+  filters: { skills?: string[]; search?: string[] };
+  updateFilters: ({ skills, search }: { skills?: string[]; search: string[] }) => void;
 }
 
